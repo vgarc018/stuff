d9826a1b53989a3785cf9b7b583ce8b0a12d0611
Virgilio Garcia <vgarc018@ucr.edu>
2015-02-26 18:52:43 -0800

    added getenv and concatenated the cmd and sorted

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -66,12 +66,35 @@ void parsing(string s, vector<string> &v)
     v.push_back(*i);
   }
 }
-
+template <typename T>
+void cat (vector<T> &s, string cmd)
+{
+  for(auto i = 0; i < s.size(); i++)
+  {
+    string temp = s[i] + "/" + cmd;
+    s[i] = temp;
+  }
+}
 void execvp_connectors(string s)
 {
-  char_separator<char> space(" ");
-  mytok tok (s, space);
-  for();
+  const char *path_name = "PATH";
+  char *path = getenv(path_name);
+  if(path == NULL)
+  {
+    perror("error in getenv");
+    exit(1);
+  }
+  string path_str(path);
+  char_separator<char> colon(":");
+  mytok tok (path_str, colon);
+  vector<string> paths;
+  for(auto i =tok.begin(); i != tok.end(); ++i)
+  {
+    paths.push_back(*i);
+  }
+  cat(paths, s);
+  sort(paths.begin(), paths.end());
+  print(paths);
 }
 
 
@@ -81,10 +104,11 @@ int main()
   vector<string> v;
   getline(cin, line);
   parsing(line, v);
-  print(v);
+  //print(v);
   queue<string> co;
   connectors(line, co);
-  qprint(co);
+  //qprint(co);
+  execvp_connectors(v[0]);
   
   return 0;
 }
